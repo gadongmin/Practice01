2312845ea628a79a8017e13107612244bce68910
gadongmin <dmkim777@naver.com>
2025-04-17 12:34:37 +0900

    20250417(1)

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+		/*
+		
+		다음중 기본 자료형중 정수형자료형이 아닌 것은?
+		(1) int - 정수형(4byte, -2147483648 ~ 2147483647)
+		(2) float - 실수형(4byte, -3.4E38 ~ 3.4E38)
+		(3) byte - 정수형(1byte, -128 ~ 127)
+		(4) long - 정수형(8byte, -9223372036854775808 ~ 9223372036854775807)
+
+		*/	
+	}
+}
